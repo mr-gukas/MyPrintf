145f801088fbca20cb5a338b84c4a1bc94d74515
mr-gukas <frodo615@yandex.ru>
2023-03-19 13:23:18 +0300

    use my printf in C program

---
+++ main.cpp
@@ -0,0 +1,8 @@
+extern "C" int gukasPrintf (const char *, ...);
+
+int main()
+{
+    gukasPrintf("My name is %s, I am %d y.o, in hex: %x, in oct: %o, in bin: %b\n also i can do %%, %t, and i love %c\n", "Vladimir", 18, 18, 18, 18, 'u');
+
+    return 0;
+}
