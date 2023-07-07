24441adc5d444160ee919c9145786b765dd60e19
mr-gukas <gukasyan.vladimir@mail.ru>
2023-07-07 21:40:09 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@ extern "C" int gukasPrintf (const char *, ...);
 
 int main()
 {
-    gukasPrintf("My name is %s, I am %d y.o, in hex: %x, in oct: %o, in bin: %b\n also i can do %%, %t, and i love %c\n", "Vladimir", -1, 18, 18, 18, 'u');
-
+    int i = -2147483648;
+    
+    for (int i = -2147483648; i <= 2147483647; ++i)
+    {
+        gukasPrintf("%d\n", i);
+    }
     return 0;
 }
